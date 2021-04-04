12e7afff4eaa552ed66847d78a7dd13002720722
Daniel Ober <ChillDrummer@MacBook-Pro.local>
2021-04-03 23:44:43 -0500

    setup modal for delete

---
+++ client/src/components/Modal.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Modal() {
+    return (
+        <div className='modal'>
+            <p>Are you sure?</p>
+            <button>no</button>
+            <button>yes</button>
+        </div>
+    )
+}
--- client/src/screens/Recipes.jsx
+++ client/src/screens/Recipes.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import {useState } from 'react'
+import Modal from '../components/Modal';
 
 export default function Recipes(props) {
+    const [open, handleOpen] = useState(false)
     const { recipes } = props;
     return (
         <div>
             <h2>Recipes</h2>
             {
                 recipes.map(recipe => (
+                    <>
                     <p key={recipe.id}>{recipe.name}</p>
+                    <button onClick={() => handleOpen(true)}>DELETE RECIPE</button>
+                    </>
                 ))
             }
+            {open && (
+                <Modal 
+                handleOpen={handleOpen}
+                />
+            )}
         </div>
     )
 }
