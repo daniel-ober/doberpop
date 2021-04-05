b04267f8b20efc3df553922c4a0b3f63872875db
Daniel Ober <ChillDrummer@MacBook-Pro.local>
2021-04-05 01:04:52 -0500

    added association of mapped ingredients setup mapped ingredients on recipe details screen

--- client/src/screens/RecipeDetails.jsx
+++ client/src/screens/RecipeDetails.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { getOneRecipe } from '../services/recipes'
 
 export default function RecipeDetails() {
+    const [recipeItem, setRecipeItem] = useState(null);
+    const { id } = useParams();
+
+    useEffect(() => {
+        const fetchRecipeItem = async () => {
+            const recipeData = await getOneRecipe(id)
+            setRecipeItem(recipeData);
+        }
+        fetchRecipeItem()
+    }, [id])
+
     return (
         <div>
-            <h2>Recipe Details</h2>
+            <h2>{recipeItem?.name}</h2>
+            {recipeItem?.ingredients.map(ingredient => (
+                <p key={ingredient.id}>{ingredient.name}</p>
+            ))}
+            
         </div>
     )
 }
