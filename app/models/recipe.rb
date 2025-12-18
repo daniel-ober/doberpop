7440934d25c52d6af9a9b61e6eb5ef118485cef6
Dan Ober <danober.dev@gmail.com>
2025-12-18 07:04:18 -0600

    Add favorites model and associations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
-  has_and_belongs_to_many :ingredients
-  belongs_to :user
+  # existing associations...
+
+  has_many :favorites, dependent: :destroy
+  has_many :favorited_by_users, through: :favorites, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ApplicationRecord
-    has_secure_password
-    has_many :recipes
-  
-    validates :username, presence: true, uniqueness: true
-    validates :email, presence: true, uniqueness: true
-    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-    validates :password, length: { minimum: 6 }
-  
-  end
+  # existing associations...
+
+  has_many :favorites, dependent: :destroy
+  has_many :favorite_recipes, through: :favorites, source: :recipe
+end
