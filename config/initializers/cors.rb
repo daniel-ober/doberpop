1fe5ece9f81c88813bbc49e57c9e8a5cb28abd52
Dan Ober <danober.dev@gmail.com>
2025-12-20 03:58:54 -0600

    Allow admin API endpoints via CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,15 +1,28 @@
-# Be sure to restart your server when you modify this file.
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
-# Read more: https://github.com/cyu/rack-cors
+# config/initializers/cors.rb
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins(
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://doberpop.com',
+      'http://doberpop.com',
+      'https://www.doberpop.com',
+      'http://www.doberpop.com'
+    )
 
-    resource '*',
+    # Auth endpoints (login/register)
+    resource '/auth/*',
+      headers: :any,
+      methods: [:post, :options]
+
+    # Public API endpoints (includes /api/recipes, favorites, etc.)
+    resource '/api/*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+
+    # (Optional but explicit) Admin endpoints
+    resource '/api/admin/*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
