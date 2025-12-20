104bf0a920bf2c12085a133c698959cea8c60535
Dan Ober <danober.dev@gmail.com>
2025-12-20 04:04:40 -0600

    Allow credentials for API CORS (admin dashboard)

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,29 +1,24 @@
 # config/initializers/cors.rb
-
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins(
-      'http://localhost:3000',
-      'http://localhost:3001',
-      'https://doberpop.com',
-      'http://doberpop.com',
-      'https://www.doberpop.com',
-      'http://www.doberpop.com'
+      "http://localhost:3000",
+      "http://doberpop.com",
+      "http://www.doberpop.com",
+      "https://doberpop.com",
+      "https://www.doberpop.com"
     )
 
-    # Auth endpoints (login/register)
-    resource '/auth/*',
-      headers: :any,
-      methods: [:post, :options]
+    # Auth routes (login / register)
+    resource "/auth/*",
+             headers: :any,
+             methods: %i[post options],
+             credentials: true
 
-    # Public API endpoints (includes /api/recipes, favorites, etc.)
-    resource '/api/*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
-
-    # (Optional but explicit) Admin endpoints
-    resource '/api/admin/*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    # All API routes (including /api/admin/*)
+    resource "/api/*",
+             headers: :any,
+             methods: %i[get post put patch delete options head],
+             credentials: true
   end
 end
