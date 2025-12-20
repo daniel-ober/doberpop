68a2c5149c1885faebe791be8502b7f3c8a58b71
Dan Ober <danober.dev@gmail.com>
2025-12-20 04:18:16 -0600

    Allow doberpop.com and www via CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,21 +3,15 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins(
-      "http://localhost:3000",
-      "http://doberpop.com",
-      "http://www.doberpop.com",
-      "https://doberpop.com",
-      "https://www.doberpop.com"
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'http://doberpop.com',
+      'https://doberpop.com',
+      'http://www.doberpop.com',
+      'https://www.doberpop.com'
     )
 
-    # Auth endpoints (login/register) – use cookies
-    resource "/auth/*",
-             headers: :any,
-             methods: %i[post options],
-             credentials: true
-
-    # All API endpoints (including /api/admin/users, /api/admin/recipes)
-    resource "/api/*",
+    resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
              credentials: true
