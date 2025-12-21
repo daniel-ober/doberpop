d747ea77ea05045c292e67a1db1a70d228842231
Dan Ober <danober.dev@gmail.com>
2025-12-21 12:43:07 -0600

    Remove bootsnap require from boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,13 @@
 # config/boot.rb
+
+# Ensure the standard Logger constant is loaded before ActiveSupport
+require 'logger'
+
+# Point Bundler at the Gemfile
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
-require 'bundler/setup'   # Set up gems listed in the Gemfile.
-require 'bootsnap/setup'  # Speed up boot time by caching expensive operations.
-require 'logger'          # Ensure Logger is loaded for Ruby 3.1 + Rails 6.1
+# Set up gems listed in the Gemfile
+require 'bundler/setup'
+
+# IMPORTANT: We are *not* loading bootsnap at all anymore.
+# No `require "bootsnap/setup"` here.
